04c33f20efa3444ad37a6a1c3e4d19d64bf9e750
Pushpa2509 <pushpar.blr@gmail.com>
2021-02-16 19:22:56 +0000

    implement excercise001 - checkHello

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -4,27 +4,23 @@ import java.util.List;
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
-        // Add your code here
-        return "";
+        return Character.toUpperCase(word.charAt(0))+ word.substring(1);
     }
 
     public String generateInitials(String firstName, String lastName) {
-        // Add your code here
         return "";
     }
 
     public double addVat(double originalPrice, double vatRate) {
-        // Add your code here
         return 0.0;
     }
 
     public String reverse(String sentence) {
-        // Add your code here
+
         return "";
     }
 
     public int countLinuxUsers(List<User> users) {
-        // Add your code here
         return 0;
     }
 }
